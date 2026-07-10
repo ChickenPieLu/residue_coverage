f40534ca4292de774ccc706651ccb57a039d0fd3
ChickenPieLu <101638144+ChickenPieLu@users.noreply.github.com>
2026-07-10 17:36:29 +0800

    add error map into predict.py

--- predict.py
+++ predict.py
@@ -3,6 +3,22 @@ import utils
 import training
 import numpy as np
 import argparse
+from sklearn.metrics import classification_report
+
+def error_map(mask, pred_mask):
+    mask = mask.astype(bool)
+    pred_mask = pred_mask.astype(bool)
+
+    tp = mask & pred_mask
+    fp = ~mask & pred_mask
+    fn = mask & ~pred_mask
+
+    result = np.zeros((*mask.shape, 3), dtype=np.uint8)
+    result[tp] = [0, 200, 0]    # green
+    result[fp] = [255, 0, 0]    # red
+    result[fn] = [0, 0, 255]    # blue
+
+    return result
 
 def predict(dirs):
     img_paths, mask_paths = utils.read_paths(dirs)
@@ -21,12 +37,19 @@ def predict(dirs):
     y = utils.tiff_read(mask_paths[img_num]).reshape(-1).astype(np.uint8)
     pred = clf.predict(X)
 
+    print(classification_report(y,pred,zero_division=0))
     training.print_segmentation_metrics(y, pred)
 
     mask = utils.tiff_read(mask_paths[img_num])
     pred_mask = pred.reshape(mask.shape)
-    result = np.stack([pred_mask*200,mask*200,np.zeros_like(pred_mask)], axis=-1)
-    utils.show_plt(np.concatenate([utils.jpg_read(img_paths[img_num]),result],axis=1))
+    errors = error_map(mask, pred_mask)
+
+    utils.show_plt(
+        np.concatenate([
+            utils.jpg_read(img_paths[img_num]),
+            errors,
+        ], axis=1)
+    )
 
 if __name__ == "__main__":
     dirs = ["residue_background/Zak-W-winterBarley_1m_20220401/IMG_0939",]
