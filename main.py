7f88b64c3a76a1806428ab2f73c95c68ec360d97
ChickenPieLu <101638144+ChickenPieLu@users.noreply.github.com>
2026-07-17 18:59:42 +0800

    model study ability verified

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from torch.utils.data import DataLoader
 import utils
 import argparse
 from dataset import ResidueDataset
@@ -13,16 +14,54 @@ def main(args):
     mask_paths = [p + ".tif" for p in file_names]
 
     dataset = ResidueDataset(img_paths, mask_paths)
-
-    img, mask = dataset[0]
-    img = img.unsqueeze(0)
-    print("Image:", img.shape)
-    print("Mask:", mask.shape)
+    loader = DataLoader(
+        dataset,
+        batch_size=2,
+        shuffle=False,
+        num_workers=0
+    )
+    imgs,masks = next(iter(loader))
 
     model = MiniUNet()
-    output = model(img)
-    print("Output:", output.shape)
-    
+    criterion = nn.BCEWithLogitsLoss()
+    optimiser = optim.Adam(
+        model.parameters(),
+        lr = 0.001
+    )
+
+    model.train()
+
+    for epoch in range(51):
+        optimiser.zero_grad()
+
+        output = model(imgs)
+        loss = criterion(output,masks)
+
+        loss.backward()
+        optimiser.step()
+
+        if epoch % 10 == 0:
+            print(f"epoch: {epoch}")
+            print(f"loss: {loss:.6f}")
+
+    model.eval()
+
+    with torch.no_grad():
+        outputs = model(imgs)
+        probability = torch.sigmoid(outputs)
+        preds = (probability>0.5).float()
+
+    intersection = (preds * masks).sum()
+    union = ((preds + masks) > 0).float().sum()
+    iou = intersection / (union + 1e-8)
+
+    print(
+        f"loss={loss.item():.4f}, "
+        f"IoU={iou.item():.4f}, "
+        f"true_ratio={masks.mean().item():.4f}, "
+        f"pred_ratio={preds.mean().item():.4f}, "
+        f"max_prob={probability.max().item():.4f}"
+    )
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
